58e7b1ec77289c5cdc10d4dc56d498fb20bdca58
JihunKong <157499262+JihunKong@users.noreply.github.com>
2024-08-12 15:25:21 +0900

    Update schoolfood.py

--- apps/schoolfood.py
+++ apps/schoolfood.py
@@ -5,8 +5,7 @@ from openai import OpenAI
 def main():
     
     # NEIS API 키 설정
-    client = OpenAI(api_key=st.secrets["neis"])
-    
+    NEIS_API_KEY = st.secrets["neis"]
     # OpenAI 클라이언트 설정
     client = OpenAI(api_key=st.secrets["openai"])
     
